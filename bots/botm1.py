a69f8666680d895b9dac268451fb1374ea658887
ch-zha <chizhang95@gmail.com>
2020-04-05 17:06:32 -0700

    verified chat_messages functionality, added sleeps in botm1 to prevent 429 error

--- bots/botm1.py
+++ bots/botm1.py
@@ -1,4 +1,5 @@
 import sys, os
+import time
 
 filename = os.path.join(os.path.dirname(__file__), '..')
 sys.path.insert(1, filename)
@@ -8,6 +9,7 @@ import json
 from configparser import ConfigParser
 from pyngrok import ngrok
 
+### Setup
 parser = ConfigParser()
 parser.read("bots/bot.ini")
 client_id = parser.get("OAuth", "client_id")
@@ -25,19 +27,36 @@ user_response = client.user.get(id='me')
 user = json.loads(user_response.content)
 print(user)
 print('---')
+time.sleep(2) # Sleep to avoid API rate limit
 
 print(json.loads(client.meeting.list(user_id="me").content))
-client.chat_channels.list()
-channels = json.loads(client.chat_channels.list().content)["channels"]
+time.sleep(2) # Sleep to avoid API rate limit
+response = json.loads(client.chat_channels.list().content)
+print(response)
+channels = response["channels"]
 print(channels)
+
 for c in channels:
     print(c)
     if "my_test" in c.values():
         print("Found channel my_test", c["id"])
         cid = to_channel = c["id"]
+
+# Send message
 stop = False
 while not stop:
     message = input("Enter message: ")
     print(client.chat_messages.post(to_channel=cid, message=message))
     if message == "stop":
         stop = True
+
+# List message
+messageList = json.loads(client.chat_messages.list(to_channel=cid, user_id="me").content)["messages"]
+time.sleep(1) # Sleep to avoid API rate limit
+
+# Update a message
+print(client.chat_messages.update(to_channel=cid, messageId=messageList[0]["id"], message="I've been edited"))
+time.sleep(2) # Sleep to avoid API rate limit
+
+# Delete a message
+print(client.chat_messages.delete(to_channel=cid, messageId=messageList[0]["id"]).content)
